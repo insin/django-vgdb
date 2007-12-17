b41781801d1d307b26f6330e0004ac8044f919c8
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2007-12-17 19:52:14 +0000

    Added comments about required tools to graph generation batch file
    darcs-hash:20071217195214-1d602-bea6f92d83cbe7295eabbd1d58fb93414f38bfd6.gz

--- generate_graph.bat
+++ generate_graph.bat
@@ -1,2 +1,4 @@
+REM Requires modelviz.py - http://unicoders.org/code/django/trunk/utils/
 modelviz.py vgdb > models.dot
+REM Requires Graphviz - http://www.graphviz.org/
 dot models.dot -Tpng -o models.png
