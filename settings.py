e09566dd767ead2deb41b07bea699a17e66dd8ca
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2007-12-17 22:52:33 +0000

    Documented the settings module and tidied some comments
    darcs-hash:20071217225233-1d602-0892b9fb59eaa303337e69315c4ca3a489da0d98.gz

--- settings.py
+++ settings.py
@@ -1,3 +1,27 @@
+"""
+Video game database settings.
+
+A ``local_settings`` module must be made available to define sensitive
+and highly installation-specific settings.
+
+The following is a template for the settings which should be provided by
+the ``local_settings`` module::
+
+   # Database settings
+   DATABASE_ENGINE = ''
+   DATABASE_NAME = ''
+   DATABASE_USER = ''
+   DATABASE_PASSWORD = ''
+   DATABASE_HOST = ''
+   DATABASE_PORT = ''
+
+   # URL that handles the media served from MEDIA_ROOT
+   MEDIA_URL = ''
+
+   # Make this unique and don't share it with anybody
+   SECRET_KEY = ''
+
+"""
 import os
 
 DIRNAME = os.path.dirname(__file__)
@@ -14,7 +38,7 @@ MANAGERS = ADMINS
 
 INTERNAL_IPS = ('127.0.0.1',)
 
-# Database settings will be obtained from local_settings module
+# Database settings will be obtained from a local_settings module
 
 # Local time zone for this installation. All choices can be found here:
 # http://www.postgresql.org/docs/current/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
@@ -27,18 +51,18 @@ LANGUAGE_CODE = 'en-gb'
 
 SITE_ID = 1
 
-# Absolute path to the directory that holds media.
+# Absolute path to the directory that holds media
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = os.path.join(DIRNAME, 'media')
 
-# MEDIA_URL setting will be obtained from local_settings module
+# MEDIA_URL setting will be obtained from a local_settings module
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
+# Examples: "http://foo.com/media/", "/media/"
 ADMIN_MEDIA_PREFIX = '/media/'
 
-# SECRET_KEY setting will be obtained from local_setting module
+# SECRET_KEY setting will be obtained from a local_settings module
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
